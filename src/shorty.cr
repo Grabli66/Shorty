ac9eb720139251e6403dc17b9643eee9b0577a00
Grabli66 <grabli66@gmail.com>
2021-02-14 21:46:53 +0300

    Fix find document by id

--- src/shorty.cr
+++ src/shorty.cr
@@ -21,7 +21,7 @@ module Shorty
     id = BSON::ObjectId.new
     id_str = id.to_s
     link_collection.insert_one({_id: id, long_link: long_link})
-    
+
     next {
       "short_link" => "https://appshorty.herokuapp.com/#{id_str}"
     }.to_json
@@ -31,8 +31,10 @@ module Shorty
   # В базе ищет по короткой ссылке длинную
   # Перенаправляет браузер на длинную ссылку
   get "/:short_link_id" do |env|
-    short_link_id = env.params.url["short_link_id"]
-    document = link_collection.find_one({_id: short_link_id})
+    short_link_id_str = env.params.url["short_link_id"]
+    short_link_id = BSON::ObjectId.new(short_link_id_str)
+    
+    document = link_collection.find_one({_id: short_link_id})    
     long_link = (document.try &.["long_link"]?).try &.to_s
     unless long_link
       halt env, status_code: 404, response: "Not found"
