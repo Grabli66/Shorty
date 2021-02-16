f3225122b1b473bdd9f5c9da48ff9f6c43f4d2f9
Grabli66 <grabli66@gmail.com>
2021-02-16 11:18:40 +0300

    Links list and show_count

--- src/shorty.cr
+++ src/shorty.cr
@@ -1,4 +1,5 @@
 require "kemal"
+require "html_builder"
 require "cryomongo"
 
 # https://cloud.mongodb.com/
@@ -7,9 +8,61 @@ require "cryomongo"
 module Shorty
   VERSION = "0.1.0"
 
+  HOST_URL = "https://appshorty.herokuapp.com"
+
   mongo_client = Mongo::Client.new "mongodb://Grabli66:NONE66@cluster0-shard-00-01.jfzer.mongodb.net:27017/shorty?authSource=admin&compressors=disabled&gssapiServiceName=mongodb&replicaSet=atlas-pqtx6g-shard-0&ssl=true"
   database = mongo_client["shorty"]
-  link_collection = database["links"]  
+  link_collection = database["links"]
+
+  # Возвращает короткую ссылку
+  def self.get_short_link(oid : String) : String
+    "#{HOST_URL}/#{oid}"
+  end
+
+  # Возвращает все ссылки
+  get "/list" do
+    cursor = link_collection.find
+    html = HTML.build do
+      table do
+        tr do
+          td do
+            text "Short Link"
+          end
+          td do
+            text "Long Link"
+          end
+          td do
+            text "Show Count"
+          end
+        end
+        cursor.each do |link|
+          tr do
+            id = link["_id"]?.try &.to_s            
+            next unless id
+            long_link = link["long_link"]?.try &.to_s
+            next unless long_link
+            show_count = link["show_count"]?.try &.as(Int32) || 0
+
+            td do
+              a(href: long_link) do
+                text get_short_link(id)
+              end
+            end
+            td do
+              a(href: long_link) do
+                text long_link
+              end
+            end
+            td do              
+                text show_count.to_s
+            end
+          end
+        end
+      end
+    end
+    # json = link.to_json
+    next html
+  end
 
   # Добавляет ссылку
   # Получает json с длинной ссылкой
@@ -20,10 +73,10 @@ module Shorty
     # TODO: сделать другой механизм короткой ссылки
     id = BSON::ObjectId.new
     id_str = id.to_s
-    link_collection.insert_one({_id: id, long_link: long_link})
+    link_collection.insert_one({_id: id, long_link: long_link, show_count: 0})
 
     next {
-      "short_link" => "https://appshorty.herokuapp.com/#{id_str}"
+      "short_link" => get_short_link(id_str),
     }.to_json
   end
 
@@ -33,12 +86,16 @@ module Shorty
   get "/:short_link_id" do |env|
     short_link_id_str = env.params.url["short_link_id"]
     short_link_id = BSON::ObjectId.new(short_link_id_str)
-    
-    document = link_collection.find_one({_id: short_link_id})    
+
+    document = link_collection.find_one({_id: short_link_id})
     long_link = (document.try &.["long_link"]?).try &.to_s
     unless long_link
       halt env, status_code: 404, response: "Not found"
     end
+    show_count = document.try &.["show_count"]?.try &.as(Int32) || 0
+    show_count += 1
+    link_collection.update_one({_id: short_link_id}, { "$set": { show_count: show_count }})    
+
     env.redirect long_link
   end
 
